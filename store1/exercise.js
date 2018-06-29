fc4bf74fa5efad0cd3c99f83ed51b3814508b32b
a25patel <aanaletap2510@gmail.com>
2018-06-29 17:29:03 -0600

    accessing complete

--- store1/exercise.js
+++ store1/exercise.js
@@ -3,24 +3,47 @@ var store1 = require('./store1-data.js');
 // *********** ACCESSING DATA *********** \\
 
 // Return the value '4.63' from store1
-function accessesingData1() {
-
+function accessesingData1(store1) {
+  var value = '4.63';
+  for (var key in store1) {
+    for (var i = 0; i < store1[key].length; i++) {
+      for (var j = 0; j < store1[key][i].length; j++) {
+        if (store1[key][i][j] === value) {
+          return value;
+        }
+      }
+    }
+  }
 }
 
+console.log(accessesingData1(store1));
 
 // Return how many 'Mint Wafers' were sold on January 6th
-function accessesingData2() {
-
+function accessesingData2(store1) {
+  var date = store1['2015-01-06'];
+  for (var i = 0; i < date.length; i++) {
+    if (date[i][0] === 'Mint Wafers') {
+      return date[i][2];
+    }
+  }
 }
+console.log(accessesingData2(store1) + ' Mint Wafers sold on Jan 06, 2015');
 
 
 // *********** LOOPING OVER DATA *********** \\
 
 
 // Create a loop to read which candies were sold by store1 on Jan 8. After simply outputting the data, try creating an array that contains the candy names.
-function loopingData1() {
-
+function loopingData1(store1) {
+  var date = store1['2015-01-08'];
+  var candy = [];
+  for (var i = 0; i < date.length; i++) {
+    candy.push(date[i][0])
+  }
+  return candy;
 }
+
+console.log(loopingData1(store1));
 
 // Create a loop to count the total number of candies sold on Jan 10 at store1. Where do you have to initialize the counter variable? Why?
 function loopingData2() {
@@ -48,16 +71,16 @@ function loopingData5() {
 // Calculate the total revenue of the candies sold by store1 on January 10th.
 // Be sure to return a number! Example `14.55`
 function challenge1() {
-  
+
 }
 
-module.exports = {
-  accessesingData1,
-  accessesingData2,
-  loopingData1,
-  loopingData2,
-  loopingData3,
-  loopingData4,
-  loopingData5,
-  challenge1
-}
+// module.exports = {
+//   accessesingData1,
+//   accessesingData2,
+//   loopingData1,
+//   loopingData2,
+//   loopingData3,
+//   loopingData4,
+//   loopingData5,
+//   challenge1
+// }
