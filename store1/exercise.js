ecd15e391bee870e7dc33ec2e57d0531f02f87be
a25patel <aanaletap2510@gmail.com>
2018-06-29 18:59:56 -0600

    store 1 complete, store2 in progress

--- store1/exercise.js
+++ store1/exercise.js
@@ -16,7 +16,6 @@ function accessesingData1(store1) {
   }
 }
 
-console.log(accessesingData1(store1));
 
 // Return how many 'Mint Wafers' were sold on January 6th
 function accessesingData2(store1) {
@@ -46,41 +45,65 @@ function loopingData1(store1) {
 console.log(loopingData1(store1));
 
 // Create a loop to count the total number of candies sold on Jan 10 at store1. Where do you have to initialize the counter variable? Why?
-function loopingData2() {
-
+function loopingData2(store1) {
+  var date = store1['2015-01-10'];
+  var count = 0;
+  for (var i = 0; i < date.length; i++) {
+    count += date[i][2];
+  }
+  return count;
 }
+
+console.log(loopingData2(store1));
 
 // Use `Object.keys()` to get an array of the dates that candies were sold at store1.
-function loopingData3() {
-
+function loopingData3(store1) {
+  return Object.keys(store1);
 }
 
+console.log(loopingData3(store1));
 
 // Use a loop to calculate the total number of candies sold at store1.
-function loopingData4() {
-
+function loopingData4(store1) {
+  var count = 0;
+  for (var key in store1) {
+    for (var i = 0; i < store1[key].length; i++) {
+      count +=  store1[key][i][2];
+    }
+  }
+  return count;
 }
 
+console.log(loopingData4(store1));
+
 // In the previous exercise, where did you have to initialize the counter variable? Why?
-function loopingData5() {
-  return 'Answer'
+function loopingData5(store1) {
+  return 'Answer';
 }
 
 // *********** CHALLENGE *********** \\
 
 // Calculate the total revenue of the candies sold by store1 on January 10th.
 // Be sure to return a number! Example `14.55`
-function challenge1() {
-
+function challenge1(store1) {
+  var date = store1['2015-01-10'];
+  var totalRevenue = 0;
+  for (var i = 0; i < date.length; i++) {
+    var quantity = date[i][2];
+    var price = parseFloat(date[i][1]);
+    totalRevenue += (price * quantity);
+  }
+  return totalRevenue.toFixed(2);
 }
+console.log(challenge1(store1));
 
-// module.exports = {
-//   accessesingData1,
-//   accessesingData2,
-//   loopingData1,
-//   loopingData2,
-//   loopingData3,
-//   loopingData4,
-//   loopingData5,
-//   challenge1
-// }
+module.exports = {
+  accessesingData1,
+  accessesingData2,
+  loopingData1,
+  loopingData2,
+  loopingData3,
+  loopingData4,
+  loopingData5,
+  challenge1
+}; 
--- store2/exercise.js
+++ store2/exercise.js
@@ -3,26 +3,37 @@ var store2 = require('./store2-data.js');
 /////////// ACCESSING DATA ///////////
 
 // Set the sale dates for 'Banana Bunches' from store2 to a variable, then return that variable
-function accessesingData1() {
-
+function accessesingData1(store2) {
+  var dates = store2['sale dates']['Banana Bunches'];
+  return dates;
 }
+
+  console.log(accessesingData1(store2));
 
 // Using a built-in array property return the total sale dates there are for store2's 'Banana Bunches'
-function accessesingData2() {
-
+function accessesingData2(store2) {
+  return accessesingData1(store2).length;
 }
+
+console.log(accessesingData2(store2));
 
 /////////// UPDATING DATA ///////////
-
 // Set a variable to equal the prices of the 'Mint Wafers' in store2. Then reassign it to a new value. When you update your new variable, does the price in the store2 data change? Return your answer as a boolean and write a comment on why or why not.
-function updatingData1() {
+function updatingData1(store2) {
+  var price = store2['inventory prices']['Mint Wafers'];
+  store2['inventory prices']['Mint Wafers'] = '4.09';
+  return price;
+};
 
-}
+console.log(updatingData1(store2));
 
 // Set a variable equal to the 'Caramel Twists' sale dates in store2. Use pop to remove one of the sale dates for store2's 'Caramel Twists'. Does your variables value match up with what is in store2? Return your answer as a boolean and write a comment on why or why not.
-function updatingData2() {
-
+function updatingData2(store2) {
+  var dates = store2['sale dates']['Caramel Twists'];
+  return dates.pop();
 }
+
+console.log(updatingData2(store2));
 
 /////////// LOOPING OVER DATA ///////////
 
@@ -32,8 +43,9 @@ function updatingData2() {
 //   '2015-01-06': 4,
 //   '2015-01-07': 3,
 // }
-function loopingData1() {
-
+function loopingData1(store2) {
+  var dates = store2['sale dates']['Caramel Twists'];
+   
 }
 
 // Iterate over store2's sale dates data to find which day had the most total number of sales. Return the date with the most sales.
@@ -54,12 +66,12 @@ function challenge1() {
 }
 
 
-module.exports = {
-  accessesingData1,
-  accessesingData2,
-  updatingData1,
-  updatingData2,
-  loopingData1,
-  loopingData2,
-  challenge1
-}
+// module.exports = {
+//   accessesingData1,
+  // accessesingData2,
+  // updatingData1,
+  // updatingData2,
+  // loopingData1,
+  // loopingData2,
+  // challenge1
+// }
